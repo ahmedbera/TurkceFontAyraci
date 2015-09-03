68b36a477fd8c413bc1eff6fdadf9bf9c1bd78b7
arkenthera <arkengraphics@gmail.com>
2015-09-03 13:27:55 +0300

    fixed things

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -7,56 +7,73 @@ using System.Drawing.Text;
 using System.IO;
 using SharpFont;
 
-
 namespace ConsoleApplication1
 {
     class Program
     {
-        public string anaKonum = Directory.GetCurrentDirectory();
+        public static string anaKonum = Directory.GetCurrentDirectory() ;
         static void Main(string[] args)
         {
-            string anaKonum = Directory.GetCurrentDirectory();
             string turkceFontlar = Path.Combine(anaKonum, "Turkce Fontlar");
             Console.WriteLine("Dosya Konumu: " + anaKonum);
             Console.WriteLine();
 
             Directory.CreateDirectory(turkceFontlar);
-            
-            var files =
-                from ktp in Directory.EnumerateFiles(anaKonum)
-                where ktp.EndsWith(".ttf") || ktp.EndsWith(".otf")
-                select ktp;
 
+            var files = Directory.GetFiles(anaKonum).Where(x => x.EndsWith(".ttf") || x.EndsWith(".otf")).Select( x => x); //This didnt fix,but looks better hue
+            Console.WriteLine("File Count: " + files.Count());
             Library kutuphane = new Library();
 
 
-            foreach (var fontInFolder in files)
+            for(int i=0; i < files.Count();i++)
             {
-                FileInfo hareketEdecekFont = new FileInfo(fontInFolder);
-                Face font = new Face(kutuphane, fontInFolder);
+                string path = files.ElementAt(i);
+                FileInfo hareketEdecekFont = new FileInfo(path);
+                Face font = kutuphane.NewFace(path, 0); //This line fixed the error
+
+
+
 
                 uint harf1 = font.GetCharIndex(287); // ğ 
                 uint harf2 = font.GetCharIndex(351); // ş
                 uint harf3 = font.GetCharIndex(231); // ç
                 uint harf4 = font.GetCharIndex(246); // ö
                 uint harf5 = font.GetCharIndex(252); // ü
-                bool uselessSwitch = false;
+
+                List<uint> charList = new List<uint> { harf1, harf2, harf3, harf4, harf5 };
+                bool hasTurkishChars = false;
+
+
                 if (harf1 == 0 || harf2 == 0 || harf3 == 0 || harf4 == 0 || harf5 == 0)
+                {
+                    hasTurkishChars = false;
+                }
+                else
+                {
+                    hasTurkishChars = true;
+                }
+                //Not working >.<
+                //for (int chitanda = 0; chitanda < charList.Count; chitanda++)
+                //{
+                //    uint gIndex = 0;
+                //    uint chCode = font.GetNextChar(harf3, out gIndex);
+                //    if (chCode != charList[chitanda])
+                //    {
+                //        hasTurkishChars = false;
+                //    }
+                //}
+                if (!hasTurkishChars)
                 {
                     Console.WriteLine(font.FamilyName + " :Türkçe Karakterler: YOK");
                 }
                 else
                 {
                     Console.WriteLine(font.FamilyName + " :Türkçe Karakterler: VAR");
-                    uselessSwitch = true;
-                }
-                if (uselessSwitch == true)
-                {
                     hareketEdecekFont.CopyTo(turkceFontlar + "\\" + hareketEdecekFont.Name, true);
                 }
+                font.Dispose();
             }
             Console.WriteLine("\nTamalandı...");
-            Console.ReadLine();
         }
     }
 }
